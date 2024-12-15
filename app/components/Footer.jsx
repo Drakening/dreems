4e6a5cc48c729ac9d254f24a5f08e1ece0edf400
Thando (Jayson) Mkhonza <95401051+Drakening@users.noreply.github.com>
2024-12-15 11:07:43 +0200

    update

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,18 +1,15 @@
-
-
 const Footer = () => {
-
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex flex-row justify-around bg-[#141414] py-4 px-4 md:flex-row md:flex-wrap md:items-center sm:justify-around sm:border-t sm:border-gray-500 sm:pt-8">
-      <div className="text-center text-white text-base w-full md:w-auto">
+    <footer className="flex flex-col md:flex-row justify-center items-center bg-[#141414] py-4 px-4 space-y-4 md:space-y-0 md:space-x-8 sm:border-t sm:border-gray-500 sm:pt-8">
+      <div className="text-center text-white text-base">
         <p>&copy; {currentYear} Dream. All Rights Reserved.</p>
       </div>
-      <div className="text-center text-white text-base w-full md:w-auto">
+      <div className="text-center text-white text-base">
         <p>Mobile: (073) 456 4343</p>
       </div>
-      <div className="text-center text-white text-base w-full md:w-auto">
+      <div className="text-center text-white text-base">
         <p>Email: Dream@gmail.com</p>
       </div>
     </footer>
