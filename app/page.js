bbee3f52d67e394d704293e8848fd0387babf642
Thando (Jayson) Mkhonza <95401051+Drakening@users.noreply.github.com>
2025-01-27 13:00:27 +0200

    chatbot

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ import Testimonials from './components/Testimonials';
 import Questions from './components/Questions';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Chatbot from './components/Chatbot'
 
 export default function Home() {
   return (
@@ -41,6 +42,7 @@ export default function Home() {
           <Contact />
         </section>
       </main>
+      <Chatbot/>
       <Footer />
     </>
   );
